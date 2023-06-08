2d6729c632d501230a3d7fb6d87256364e98a44a
Mark <marklajom.work@gmail.com>
2023-06-08 08:31:05 +0200

    Added fonts and sticky navbar

--- portfolio/src/components/LSide.tsx
+++ portfolio/src/components/LSide.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LSide = () => {
-  return <div className="hidden md:hidden lg:block">LSide</div>;
+  return (
+    <div className="hidden md:hidden lg:block bottom-0 fixed left-0 ">
+      LSide
+    </div>
+  );
 };
 
 export default LSide;
