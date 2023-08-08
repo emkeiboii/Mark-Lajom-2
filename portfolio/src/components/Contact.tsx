f6f4a79ebd6aeb1052b42cc9211abdc5543538c1
Mark <marklajom.work@gmail.com>
2023-08-08 18:01:17 +0200

    Added Fade effect to Contact

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import Fade from "react-reveal/Fade";
+
 const Contact = () => {
   return (
     <div
@@ -6,42 +8,48 @@ const Contact = () => {
     >
       <div className="">
         <form className="flex flex-col gap-5">
-          <h1 className="font-bold text-4xl w-80">Contact Form</h1>
-          <input
-            type="text"
-            name="Name"
-            placeholder="Full Name"
-            className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
-            required
-          />
-          <input
-            type="email"
-            name="Email"
-            placeholder="Email"
-            className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
-            required
-          />
-          <textarea
-            name="Message"
-            placeholder="Message"
-            className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
-            required
-          ></textarea>
-          <button
-            type="submit"
-            className="align-self outline outline-1 rounded text-lavender py-2 my-4 px-5 hover:bg-lavender hover:bg-opacity-20 duration-200 w-32 ease-out"
-          >
-            Send
-          </button>
+          <Fade>
+            <h1 className="font-bold text-4xl w-80">Contact Form</h1>
+            <input
+              type="text"
+              name="Name"
+              placeholder="Full Name"
+              className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
+              required
+            />
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
+              required
+            />
+            <textarea
+              name="Message"
+              placeholder="Message"
+              className="flex-1 w-full p-4 text-navy border rounded-lg sm:text-md focus:ring-navy focus:border-navy dark:bg-gray-700 "
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="align-self outline outline-1 rounded text-lavender py-2 my-4 px-5 hover:bg-lavender hover:bg-opacity-20 duration-200 w-32 ease-out"
+            >
+              Send
+            </button>
+          </Fade>
         </form>
       </div>
-      <hr className="w-0.5 lg:h-72 bg-white hidden sm:hidden lg:block ease-in-out duration-700" />
+      <Fade bottom>
+        <hr className="w-0.5 lg:h-72 bg-white hidden sm:hidden lg:block ease-in-out duration-700" />
+      </Fade>
       <div>
-        <h1 className="font-bold text-4xl sm:text-4xl md:text-6xl lg:text-7xl text-white py-5 hover:text-blue duration-500 ease-in-out ">
-          LETS <br />
-          CREATE <br />
-          TOGETHER!
-        </h1>
+        <Fade>
+          <h1 className="font-bold text-4xl sm:text-4xl md:text-6xl lg:text-7xl text-white py-5 hover:text-blue duration-500 ease-in-out ">
+            LETS <br />
+            CREATE <br />
+            TOGETHER!
+          </h1>
+        </Fade>
       </div>
     </div>
   );
