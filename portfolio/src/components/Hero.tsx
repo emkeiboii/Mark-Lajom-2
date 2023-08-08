0b159aa934e10995a88a1c749d9276560482ffc2
Mark <marklajom.work@gmail.com>
2023-08-08 17:55:36 +0200

    Added Fade effect to Hero

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll";
+import Fade from "react-reveal/fade";
 
 const Hero = () => {
   return (
@@ -8,31 +9,33 @@ const Hero = () => {
       className="py-48 px-4 sm:px-0 md:px-0 lg:px-28 flex items-center justify-center flex-col"
     >
       <div>
-        <h2 className="font-mono text-blue">Hey! My name is:</h2>
-        <h1 className="font-extrabold text-4xl md:text-6xl lg:text-7xl -mx-1  text-blue  ">
-          Mark Lajom
-        </h1>
-        <h3 className="font-extrabold text-4xl md:text-6xl lg:text-7xl mb-3">
-          I'm a front-end web developer
-        </h3>
-        <p className="text-grey">
-          I love coding, cooking and eating. I'm inspired by creativity and
-          driven by passion
-        </p>
-        <button className="font-mono outline  outline-1 rounded text-lavender py-2 px-5 hover:bg-lavender hover:bg-opacity-20 duration-200 ease-out my-3 mr-4">
-          <Link
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            Contact me!
-          </Link>
-        </button>
-        <button className="font-mono outline  outline-1 rounded text-blue py-2 px-5 hover:bg-blue hover:bg-opacity-20 duration-200 ease-out my-3 mr-4 md:hidden lg:hidden">
-          CV
-        </button>
+        <Fade>
+          <h2 className="font-mono text-blue">Hey! My name is:</h2>
+          <h1 className="font-extrabold text-4xl md:text-6xl lg:text-7xl -mx-1  text-blue  ">
+            Mark Lajom
+          </h1>
+          <h3 className="font-extrabold text-4xl md:text-6xl lg:text-7xl mb-3">
+            I'm a front-end web developer
+          </h3>
+          <p className="text-grey">
+            I love coding, cooking and eating. I'm inspired by creativity and
+            driven by passion
+          </p>
+          <button className="font-mono outline  outline-1 rounded text-lavender py-2 px-5 hover:bg-lavender hover:bg-opacity-20 duration-200 ease-out my-3 mr-4">
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Contact me!
+            </Link>
+          </button>
+          <button className="font-mono outline  outline-1 rounded text-blue py-2 px-5 hover:bg-blue hover:bg-opacity-20 duration-200 ease-out my-3 mr-4 md:hidden lg:hidden">
+            CV
+          </button>
+        </Fade>
       </div>
     </div>
   );
