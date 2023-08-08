e66227940ecebc97bc5652a5ed17fd53fdef45db
Mark <marklajom.work@gmail.com>
2023-08-08 17:51:47 +0200

    Added effects to left, right side

--- portfolio/src/components/RSide.tsx
+++ portfolio/src/components/RSide.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-
+import Fade from "react-reveal/fade";
 const RSide = () => {
   return (
     <div className="hidden md:hidden lg:flex  bottom-0 fixed right-10 w-2 [writing-mode:vertical-rl] justify-center items-center text-white gap-2">
-      <a
-        href=""
-        className="font-mono  tracking-widest text-xs hover:-translate-y-1 duration-300 hover:text-blue"
-      >
-        marklajom.work@gmail.com
-      </a>
-      <hr className="w-2/12 h-20 bg-white" />
+      <Fade bottom cascade>
+        <a
+          href=""
+          className="font-mono  tracking-widest text-xs hover:-translate-y-1 duration-300 hover:text-blue"
+        >
+          marklajom.work@gmail.com
+        </a>
+        <hr className="w-2/12 h-20 bg-white" />
+      </Fade>
     </div>
   );
 };
